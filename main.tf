e0dfb26d58d2177d9a78e90f003fb15f53bce59b
jamsheerc <134119635+jamsheerc@users.noreply.github.com>
2024-01-22 17:09:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-    required_version = "~> 1.5.2" // added latest version
+    required_version = "~> 1.7.0" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
